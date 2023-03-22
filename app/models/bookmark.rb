9043a6f0bbb4e8b630a78172cb592f8b0523b382
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-03-22 11:56:30 +0100

    active storage setup

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,5 @@
 class Bookmark < ApplicationRecord
   belongs_to :invader
   belongs_to :user
+  has_many_attached :photos
 end
--- app/models/invader.rb
+++ app/models/invader.rb
@@ -1,4 +1,5 @@
 class Invader < ApplicationRecord
   belongs_to :city
   has_many :bookmarks
+  has_one_attached :photo
 end
