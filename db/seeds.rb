211aed44c518fa4859f62e74a0a40b1a882ef680
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-03-29 19:44:48 +0200

    Seed Cities

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,24 +13,24 @@ paris = City.new(
 paris.save!
 puts "Created Paris"
 
-puts "Creating Paris Invaders"
-file = File.open("db/paris_invaders.xml")
-document = Nokogiri::XML(file)
-document.root.xpath("Placemark").each do |placemark|
-  name = placemark.xpath("name").text
-  longitude = placemark.xpath("Point/coordinates").text.gsub(' ', '').gsub(/\n/, '').split(',')[0]
-  latitude = placemark.xpath("Point/coordinates").text.gsub(' ', '').gsub(/\n/, '').split(',')[1]
-  status = placemark.xpath("description").text
-  Invader.create!(
-    name: name,
-    longitude: longitude,
-    latitude: latitude,
-    status: status,
-    city: paris
-  )
-  puts "#{name} / #{longitude} / #{latitude} / #{status}"
-end
-puts "Created Paris Invaders"
+# puts "Creating Paris Invaders"
+# file = File.open("db/paris_invaders.xml")
+# document = Nokogiri::XML(file)
+# document.root.xpath("Placemark").each do |placemark|
+#   name = placemark.xpath("name").text
+#   longitude = placemark.xpath("Point/coordinates").text.gsub(' ', '').gsub(/\n/, '').split(',')[0]
+#   latitude = placemark.xpath("Point/coordinates").text.gsub(' ', '').gsub(/\n/, '').split(',')[1]
+#   status = placemark.xpath("description").text
+#   Invader.create!(
+#     name: name,
+#     longitude: longitude,
+#     latitude: latitude,
+#     status: status,
+#     city: paris
+#   )
+#   puts "#{name} / #{longitude} / #{latitude} / #{status}"
+# end
+# puts "Created Paris Invaders"
 
 puts "Creating Fontainebleau"
 fontainebleau = City.new(
@@ -51,3 +51,73 @@ lille = City.new(
 )
 lille.save!
 puts "Created Lille"
+
+puts "Creating Nantes"
+lille = City.new(
+  name: "Nantes",
+  short: "NAN",
+  country: "France",
+  address: "Nantes, France"
+)
+lille.save!
+puts "Created Nantes"
+
+puts "Creating Marseille"
+lille = City.new(
+  name: "Marseille",
+  short: "MARS",
+  country: "France",
+  address: "Marseille, France"
+)
+lille.save!
+puts "Created Marseille"
+
+puts "Creating London"
+lille = City.new(
+  name: "London",
+  short: "LDN",
+  country: "United Kingdom",
+  address: "London, United Kingdom"
+)
+lille.save!
+puts "Created London"
+
+puts "Creating Basel"
+lille = City.new(
+  name: "Basel",
+  short: "BSL",
+  country: "Switzerland",
+  address: "Basel, Switzerland"
+)
+lille.save!
+puts "Created Basel"
+
+puts "Creating Cap Ferret"
+lille = City.new(
+  name: "Cap Ferret",
+  short: "CAPF",
+  country: "France",
+  address: "Cap Ferret, France"
+)
+lille.save!
+puts "Created Cap Ferret"
+
+puts "Creating Versailles"
+lille = City.new(
+  name: "Versailles",
+  short: "VRS",
+  country: "France",
+  address: "Versailles, France"
+)
+lille.save!
+puts "Created Versailles"
+
+puts "Creating Lyon"
+lille = City.new(
+  name: "Lyon",
+  short: "LY",
+  country: "France",
+  address: "Lyon, France"
+)
+lille.save!
+puts "Created Lyon"
