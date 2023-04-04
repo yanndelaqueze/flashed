c61d2093db5635d31dcf7ccf62b4fb843a1ab633
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-04-04 10:33:37 +0200

    coordinates

---
+++ app/javascript/controllers/user_location_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="user-location"
+export default class extends Controller {
+  static targets = ["coordinates"]
+
+  connect() {
+    console.log("user location controller is live")
+  }
+
+  getCoordinates(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition((info) => {
+      const lat = info.coords.latitude;
+      const lon = info.coords.longitude;
+      console.log(`${lat}, ${lon}`);
+      this.coordinatesTarget.innerText = `Coordinates : ${lat}, ${lon}`
+    });
+  }
+}
