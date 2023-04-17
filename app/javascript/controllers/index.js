0241e6c3330304ab73c081a6d18ed061d3657482
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-04-17 16:23:30 +0200

    modal

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,13 +2,13 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
 
-import MapController from "./map_controller"
-application.register("map", MapController)
+import MapController from "./map_controller";
+application.register("map", MapController);
 
-import UserLocationController from "./user_location_controller"
-application.register("user-location", UserLocationController)
+import UserLocationController from "./user_location_controller";
+application.register("user-location", UserLocationController);
